4846952bac896f569b2a3534f52eefed2cf813da
Christina_Sereday_ptc <cs64@students.ptcollege.edu>
2022-06-07 18:27:43 -0400

    Test LoginManager as singleton class

---
+++ app/src/main/java/net/rideshare_ptc/LoginManager.java
@@ -0,0 +1,65 @@
+package net.rideshare_ptc;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//This is one big experiment I have never done before.
+public class LoginManager {
+    //The purpose of this class is to manage the one and only logged in user.
+    //Plan is to create a list/ array which will hold the logged in user upon their login
+    //and remove it when they log off, and ideally prevent other users from logging in if
+    //the list already has the 1 user. (i.e. user in list is logged in until some condition
+    //logs them off and removes them from the list).
+    //This is creating a 'singleton' which creates this class and keeps the resulting object instance
+    //accessible throughout the entire program, supposedly.
+    //This class holds the variable list which will monitor and handle user login/logoff activity.
+
+    //Trying this as a 'singleton' and creating the instance of this class at load time
+    private static LoginManager obj = new LoginManager();
+    //The variable that the singleton object will maintain (the list)
+    List<User> loggedInUsers = new ArrayList<User>(); // size of this list should be 1 or 0 always.
+
+    public List<User> getLoggedInUsers() {
+        return loggedInUsers;
+    }
+
+    public String setLoggedInUsers(User loggedInUser) { //attempt login user taking the User obj as param.
+        String response = "";
+        if (this.loggedInUsers.size() ==1){
+            response = "Current user must logout first"; //do not add to list, return msg
+        }
+        else if (this.loggedInUsers.size() ==0){
+            loggedInUsers.add(loggedInUser);  //add the user to the list
+            response = "User added to the list successfully"; //return success message
+        }
+        return response;
+    }
+
+    public String removeLoggedInUsers(User loggedInUser) { //attempt login user taking the User obj as param.
+        String response = "";
+        if (this.loggedInUsers.size() ==1){
+            loggedInUsers.clear();
+            response = "User successfully logged out"; //do not add to list, return msg
+        }
+        else if (this.loggedInUsers.size() ==0){
+            //do nothing
+            response = "No user is logged in"; //return success message
+        }
+        return response;
+    }
+
+
+    public static LoginManager getInstance(){  //this is how you access this singleton object throughout the program
+        return obj;
+        //call by "LoginManager mgr = LoginManager.getInstance();"
+        //and then calling the functions of this instance to update it (like mgr.setLoggedInUsers(newUser));
+        //reference: https://techvidvan.com/tutorials/java-singleton-class/
+        //refereence: https://www.programiz.com/java-programming/singleton
+    }
+
+    private LoginManager(){
+        //this default constructor does nothing except create the Login manager
+    }
+
+}
