e6ad1a31ab01a26a5daf919605fb9a5c0e40a031
Christina_Sereday <christina.sereday@gmail.com>
2022-06-20 16:54:08 -0400

    Added Shell RideDetailsActivity to display individual rides

--- app/src/main/java/net/rideshare_ptc/MainMenu.java
+++ app/src/main/java/net/rideshare_ptc/MainMenu.java
@@ -13,12 +13,22 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button btnMyRides = (Button) findViewById(R.id.btnMenuViewRides);
         Button btnReqRide = (Button) findViewById(R.id.btnMenuReq);
         Button btnLogout = (Button) findViewById(R.id.btnMenuLogOut);
         Button btnPostRide  = (Button) findViewById(R.id.btnMenuPost);
         Button btnUserProf = (Button) findViewById(R.id.btnMenuViewProf);
 
 //Buttons for MyRides, All Rides, and User Profile TBA
+        btnMyRides.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager mgr = LoginManager.getInstance();
+                //startActivity(new Intent(MainMenu.this,DriverOnlySplash.class).putExtra("Success Ride Posted","User Logged out: "+ mgr.getLoggedInUserList().toString()));
+                Intent intent = new Intent(v.getContext(), RideDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
 
         btnPostRide.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +55,6 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(new Intent(MainMenu.this,MainActivity.class));
             }
         });
-        //Testing button- Route updated to UserProfile
         btnUserProf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
