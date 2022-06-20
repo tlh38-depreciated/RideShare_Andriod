a3874be0a8e736cd1317ca38f2334027f4a3bde3
Christina_Sereday <christina.sereday@gmail.com>
2022-06-20 15:50:05 -0400

    Added more/fix constraints to carDetailsActivity

---
+++ app/src/main/java/net/rideshare_ptc/RideDetailsActivity.java
@@ -0,0 +1,14 @@
+package net.rideshare_ptc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RideDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_details);
+    }
+}
